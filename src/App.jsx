fca87b3fdb2197b9e783af45a4e776bfd27b04ef
jayajput30 <rajputjay901@gmail.com>
2023-08-13 01:35:07 +0530

    feat: set up vite + general styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <h1>MCR 11</h1>
+      <p>hello world</p>
+      <button>hello</button>
+    </div>
+  );
+}
+
+export default App;
